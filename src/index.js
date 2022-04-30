8d4ff0f47ecace8547aa215aeb1e140a02318bf6
latinogan <roger.castillo.arce@gmail.com>
2022-04-30 18:31:17 -0500

    fixed linters errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
-/*import './index.css';*/
+
+import './index.css';
 
 const taskinput = document.querySelector('.insert-text');
 const content = document.querySelector('.content');
@@ -35,34 +35,34 @@ function showTodo() {
 showTodo();
 
 function updateStatus(selectedTask) {
-    /* console.log(selectedTask) */
-      // getting paragraph that contains task name
-      const taskName = selectedTask.parentElement.childNodes[3];// esto deberia subrayar el texto
-      if (selectedTask.checked) {
-        taskName.classList.add('checked');
-        // optional for the delted button completed
-        todos[selectedTask.id].completed = 'completed';
-      } else {
-        taskName.classList.remove('checked');
-        todos[selectedTask.id].completed = 'pending';
-      }
-      localStorage.setItem('todo-list', JSON.stringify(todos));
-    }
+  /* console.log(selectedTask) */
+  // getting paragraph that contains task name
+  const taskName = selectedTask.parentElement.childNodes[3]; // esto deberia subrayar el texto
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    // optional for the delted button completed
+    todos[selectedTask.id].completed = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].completed = "false";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+}
 
 function showMenu(selectedTask) {
   // console.log(selectedTask);
   const taskMenu = selectedTask.parentElement.childNodes[3];
-  taskMenu.classList.add('show');
-  document.addEventListener('click', (e) => {
+  taskMenu.classList.add("show");
+  document.addEventListener("click", (e) => {
     // removing show class from task menu on the document click
-    if (e.target.tagName !== 'I' || e.target !== selectedTask) {
-      taskMenu.classList.remove('show');
+    if (e.target.tagName !== "I" || e.target !== selectedTask) {
+      taskMenu.classList.remove("show");
     }
   });
 }
 
 function editTask(taskId, taskName) {
-//  console.log(taskId ,taskName)
+  //  console.log(taskId ,taskName)
   editId = taskId;
   isEditedTask = true;
   taskinput.value = taskName;
@@ -71,28 +71,30 @@ function editTask(taskId, taskName) {
 function deleteTask(deleteId) {
   // removing selected array
   todos.splice(deleteId, 1);
-  localStorage.setItem('todo-list', JSON.stringify(todos));
+  localStorage.setItem("todo-list", JSON.stringify(todos));
   showTodo();
 }
 
-
-
-taskinput.addEventListener('keyup', (e) => {
+taskinput.addEventListener("keyup", (e) => {
   const userTask = taskinput.value.trim();
-  if (e.key === 'Enter' && userTask) {
+  if (e.key === "Enter" && userTask) {
     if (!isEditedTask) {
       if (!todos) {
         todos = [];
       }
-      const taskInfo = { description: userTask, completed: 'false', index: todos.length + 1 };
+      const taskInfo = {
+        description: userTask,
+        completed: "false",
+        index: todos.length + 1,
+      };
       todos.push(taskInfo); // adding new task to todos abj
     } else {
       isEditedTask = false;
       todos[editId].description = userTask;
     }
 
-    taskinput.value = ' ';
-    localStorage.setItem('todo-list', JSON.stringify(todos));
+    taskinput.value = " ";
+    localStorage.setItem("todo-list", JSON.stringify(todos));
     showTodo();
   }
 });
